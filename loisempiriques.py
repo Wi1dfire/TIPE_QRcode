0018578961b4f23337b3c88d29cbbddcf19790ee
Wi1dfire <wildfire20043004@gmail.com>
2025-01-18 21:51:45 +0100

    début des fonctions récoltant les données empiriques

--- loisempiriques.py
+++ loisempiriques.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 import os
 import csv
 
-os.chdir('./image_test_QRcode/baseempirique/QRcode')
+os.chdir('./image_test_QRcode/empirique/QRcode')
 
 def base_de_Qrcode():
     for i in range(40):
@@ -16,7 +16,7 @@ def base_de_Qrcode():
 def alignements_empiriques():
     data = [["Version","liste des centres d'alignements"]]
     for i in range(40):
-        img = mpimg.imread("qrcodeV"+str(i)+".png")
+        img = mpimg.imread("qrcodeV"+str(i)+".png").tolist()
         img = iQTL.recalibrage(img)
         data.append([i, fu.loc_alignment(img)])
     with open('alignements.csv', 'w', newline='') as file:
@@ -24,7 +24,13 @@ def alignements_empiriques():
         writer.writerows(data)
 
 def main():
-    pass
+    qrcode = qr.QRCode( version=1, error_correction=qr.constants.ERROR_CORRECT_L, box_size=1, border=0)
+    img = qrcode.make_image(fill='black', back_color='white')
+    img.save("qrcodeV"+str(1)+".png")
+    image = mpimg.imread("qrcodeV"+str(1)+".png").tolist()
+    image = iQTL.recalibrage(image)
+    fu.affiche_image(image)
+    fu.affiche(image)
 
 if __name__ == "__main__":
     main()
