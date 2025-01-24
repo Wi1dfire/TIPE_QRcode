24793dff78c59802da8236486b1150643e11eba1
Wi1dfire <wildfire20043004@gmail.com>
2025-01-24 22:17:10 +0100

    créé un QRcode à partir d'une str

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import fonctionsbase as fb
 import structure as st
 import fonctionsutiles as fu
 import mask as mk
+import construction as co
 # 0 = noir et 1 = blanc
 
 def reedsolomon(bits:list, lvl : int) -> list:
@@ -69,15 +70,29 @@ def encode_info(L:str, lvl:int) -> list:
     data = fb.listtobits(data) #on convertit les données en bits
     return data
 
+def QRcode(S:str, lvl:int) -> list:
+    """(GC) Encode les informations dans un QRcode
+
+    Args:
+        S (str): information à encoder
+        lvl (int): niveau de correction
+    """
+    data = encode_info(S, lvl)
+    L = co.construit(data)
+    L = fu.ecriture(L, data)
+    mk.maskoptimal(L)
+    fu.affiche_image(L)
+    return L
+
 def main():
-    L = st.Gen_QRcode(29,True)
+    QRcode("Hello World", 30)
+    """L = st.Gen_QRcode(29)
     n=len(L)-1
     alignement = st.alignment()
     L = st.insert(L,alignement,(n-8,n-8))
     c = fu.cases_interdites(L)
-    #mk.appli(L, 101, c)
     K = []
-    print(typeinfo("10012845640"))
+    print(typeinfo("10012845640"))"""
     """
     for i in range(72):
         K.append([1,1,0,0,1,0,1,1])
@@ -90,14 +105,8 @@ def main():
         K.append([18,17,16,15,14,13,12,11])
     données = fb.octetstoliste(K)
     fu.ecriture(L,données)
-    fu.affiche_image(L)
+    fu.affiche_image_rainbow(L)
     """
-    """Helloworld_rs = encode_info("Hello World", 7, "alphanumérique", None)
-    fu.encode(L,Helloworld_rs,101,None,None)
-    fu.affiche_image(L)"""
-    #print(len(reedsolomon([1,0,0,1,0,1,1,0], 7))%8)
-    #print(len(encode_info("Hello World", 7))%8)
-    #print(len(fb.str_to_bits("Hello World"))//8)
 
 # print("Done importing")
 if __name__ == "__main__":
