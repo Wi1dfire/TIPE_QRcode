96b2dafe3d658d27d421b5fd046d03e53be4cac6
Wi1dfire <wildfire20043004@gmail.com>
2025-01-24 22:11:32 +0100

    ajout de l'espace disponible

--- loisempiriques.py
+++ loisempiriques.py
@@ -1,11 +1,10 @@
 import image_QRcode_to_liste as iQTL
 import fonctionsutiles as fu
+import fonctionsbase as fb
 import qrcode as qr
 import matplotlib.image as mpimg
 import os
 import csv
-
-os.chdir('./image_test_QRcode/empirique')
 
 def base_de_Qrcode():
     os.chdir('./QRcode')
@@ -17,20 +16,30 @@ def base_de_Qrcode():
 def alignements_empiriques(img:list)->list:
     return fu.loc_alignment(img)
 
+def espace_empirique(img:list)->list:
+    return len(img)**2 - len(fb.unique(fu.cases_interdites(img)))
+
 def tocsv(data:list)->None:
     with open('loisempiriques.csv', 'w', newline='') as file:
         writer = csv.writer(file,delimiter=';')
         writer.writerows(data)
 
+def remove_files():
+    for i in range(1,41):
+        os.remove("./QRcode/qrcodeV"+str(i)+".png")
+
 def main():
+    os.chdir('./image_test_QRcode/empirique')
     if len(os.listdir("./QRcode")) != 40:
         base_de_Qrcode()
-    data = [['versions','alignements','taille']]
+    data = [['versions','alignements','taille','espace']]
     for i in range(1,41):
         img = mpimg.imread("./QRcode/qrcodeV"+str(i)+".png").tolist()
         img = iQTL.recalibrage(img)
-        data.append([i,alignements_empiriques(img),len(img)])
+        data.append([i,alignements_empiriques(img),len(img), espace_empirique(img)])
     tocsv(data)
+    if len(os.listdir("./QRcode")) == 40:
+        remove_files()
 
 if __name__ == "__main__":
     main()
