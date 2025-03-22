2aa0da503930f505f09e19f5ee397035f751c692
Wi1dfire <wildfire20043004@gmail.com>
2025-03-22 17:21:37 +0100

    ajout de commentaire

--- loisempiriques.py
+++ loisempiriques.py
@@ -7,6 +7,9 @@ import os
 import csv
 
 def base_de_Qrcode():
+    """
+    Crée une base de 40 QRcodes de version 1 à 40
+    """
     os.chdir('./QRcode')
     for i in range(1,41):
         qrcode = qr.QRCode( version=i, error_correction=qr.constants.ERROR_CORRECT_L, box_size=1, border=0)
@@ -14,31 +17,57 @@ def base_de_Qrcode():
         img.save("qrcodeV"+str(i)+".png")
 
 def alignements_empiriques(img:list)->list:
+    """
+    renvoie l'emplacement des alignements dans l'image
+
+    Args:
+        img (list): QRcode
+
+    Returns:
+        list: liste des emplacement des alignements
+    """
     return fu.loc_alignment(img)
 
-def espace_empirique(img:list)->list:
+def espace_empirique(img:list)->int:
+    """
+    retourne nombre de cases utilisables dans le QRcode
+
+    Args:
+        img (list): QRcode
+
+    Returns:
+        int: nombre de cases utilisables
+    """
     return len(img)**2 - len(fb.unique(fu.cases_interdites(img)))
 
 def tocsv(data:list)->None:
+    """créé un fichier csv avec les données
+
+    Args:
+        data (list): liste des données
+    """
     with open('loisempiriques.csv', 'w', newline='') as file:
         writer = csv.writer(file,delimiter=';')
         writer.writerows(data)
 
 def remove_files():
+    """
+    supprime les fichiers de la base de QRcodes
+    """
     for i in range(1,41):
         os.remove("./QRcode/qrcodeV"+str(i)+".png")
 
 def main():
-    os.chdir('./image_test_QRcode/empirique')
-    if len(os.listdir("./QRcode")) != 40:
+    os.chdir('./image_test_QRcode/empirique') #on se place dans le bon répertoire
+    if len(os.listdir("./QRcode")) != 40: #on vérifie si la base de QRcodes est complète
         base_de_Qrcode()
-    data = [['versions','alignements','taille','espace']]
-    for i in range(1,41):
-        img = mpimg.imread("./QRcode/qrcodeV"+str(i)+".png").tolist()
-        img = iQTL.recalibrage(img)
-        data.append([i,alignements_empiriques(img),len(img), espace_empirique(img)])
-    tocsv(data)
-    if len(os.listdir("./QRcode")) == 40:
+    data = [['versions','alignements','taille','espace']] #on initialise la liste des données
+    for i in range(1,41): #on parcourt les QRcodes
+        img = mpimg.imread("./QRcode/qrcodeV"+str(i)+".png").tolist() #on charge l'image
+        img = iQTL.recalibrage(img) #on la recalibre pour la rendre exploitable par le programme
+        data.append([i,alignements_empiriques(img),len(img), espace_empirique(img)])#on ajoute les données à la liste des données
+    tocsv(data) #on convertie les données en csv
+    if len(os.listdir("./QRcode")) == 40: #on vérifie si la base de QRcodes est complète pour la supprimer
         remove_files()
 
 if __name__ == "__main__":
