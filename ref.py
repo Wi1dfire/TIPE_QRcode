da2a5e1cc7cf34ad0aba58aeb0a46801bb4c7cf8
Wi1dfire <wildfire20043004@gmail.com>
2025-05-25 17:10:32 +0200

    sert de référence pour trouver ce qui ne vas pas

---
+++ ref.py
@@ -0,0 +1,24 @@
+import qrcode as qr
+import os
+import matplotlib.image as mpimg
+import image_QRcode_to_liste as iQTL
+import fonctionsutiles as fu  
+
+os.chdir('./image_test_QRcode')
+if "Le QRcode_V1L.png" not in os.listdir():
+    qrcode = qr.QRCode(
+        version=1,
+        error_correction=qr.constants.ERROR_CORRECT_L,
+        box_size=1,
+        border=5,
+        mask_pattern=5  # 101 en binaire = 5 en décimal
+    )
+    qrcode.add_data("Le QRcode")
+    qrcode.make(fit=True)
+    image = qrcode.make_image(fill='black', back_color='white')
+    image.save("Le QRcode_V1L.png")
+img = mpimg.imread("Le QRcode_V1L.png").tolist()
+img = iQTL.recalibrage(img)
+img = fu.negatif(img)
+fu.affiche_image(img)
+print(fu.masque_utilise(img))
