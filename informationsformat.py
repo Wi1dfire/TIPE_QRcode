3dc35a63df0b4b451cddaeafc219c5ba2d5b65e8
Wi1dfire <wildfire20043004@gmail.com>
2025-05-30 10:51:10 +0200

    deplacement de la fct recupinformat

--- informationsformat.py
+++ informationsformat.py
@@ -33,6 +33,24 @@ def informationsformat(cor:list, mask:list) -> list:
     """
     return fb.XORlist(cor + mask + correctionbit(cor, mask),msq)
 
+def recupinformat(L:list) -> list:
+    """récupère les informations de format du QRcode
+
+    Args:
+        L (list): QRcode
+
+    Returns:
+        list: informations de format
+    """
+    info = []
+    for i in range(9):
+        info.append(L[i][8])
+    for i in range(7, 15):
+        info.append(L[8][14-i])
+    info.pop(6)
+    info.pop(9)
+    return fb.XORlist(info[::-1], [1,0,1,0,1,0,0,0,0,0,1,0,0,1,0]) #on récupère les informations de format en appliquant le masque de format
+
 def masqueinformat() -> list:
     """retourne le masque de format
 
